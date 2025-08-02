ce7e7dd2d3cb2f1577c179ee40ceba503a8a90c3
Unmesh Bari <unmeshbari5@gmail.com>
2025-08-02 15:44:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+function daysOfAYear(year) {
+  if (typeof year !== "number" || year <= 0 || !Number.isInteger(year)) {
+    throw new Error("Invalid year");
+  }
+
+  const isLeap = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+  return isLeap ? 366 : 365;
+}
